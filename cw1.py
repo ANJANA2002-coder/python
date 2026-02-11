680dd80bf1ee40fae806ae06b80212d75351c24e
ANJANA2002-coder <gnairanjana1@gmail.com>
2026-02-11 13:10:50 +0530

    cw of day 1

---
+++ cw1.py
@@ -0,0 +1,13 @@
+apple=15.5
+orange=20
+grape=10.25
+total=apple+orange+grape
+print(total)
+Totalvolume=int(total)
+print(Totalvolume)
+a=str(Totalvolume)
+print("The total cost is: ",a)
+import random
+random_number=random.randrange(5,10)
+print(random_number)
+print(Totalvolume+random_number)
